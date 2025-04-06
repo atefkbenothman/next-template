7d7d9490fb82451238bbf5f3197045f25cc34578
Atef Kai Benothman <atefkbenothman@gmail.com>
2025-04-05 20:55:40 -0700

    chore(general): minor changes

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link"
 
-export default function Home() {
+export default function Settings() {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
       <div className="flex flex-col">
-        <p>Home</p>
-        <Link href="/settings" className="w-fit underline" prefetch={true}>
-          Settings
+        <p>Settings Page</p>
+        <Link href="/" className="w-fit underline">
+          Home
         </Link>
       </div>
     </div>
