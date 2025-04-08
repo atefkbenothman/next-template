82210498d3a6f6374c1a5ebfce29f97fde1dd64d
Atef Kai Benothman <atefkbenothman@gmail.com>
2025-04-08 02:03:10 -0700

    feat(db): add supabase

--- src/env.js
+++ src/env.js
@@ -10,11 +10,15 @@ export const env = createEnv({
   },
   client: {
     NEXT_PUBLIC_CLIENT_API_KEY: z.string(),
+    NEXT_PUBLIC_SUPABASE_URL: z.string(),
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string()
   },
   runtimeEnv: {
     SERVER_API_KEY: process.env.SERVER_API_KEY,
     NODE_ENV: process.env.NODE_ENV,
     NEXT_PUBLIC_CLIENT_API_KEY: process.env.NEXT_PUBLIC_CLIENT_API_KEY,
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
